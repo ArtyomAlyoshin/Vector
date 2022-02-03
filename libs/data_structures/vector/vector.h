eb62e0cddc4dce7d8b8cd1ba3a393966c704cb14
Artyom <artem_aleshin_2003@mail.ru>
2022-02-03 17:48:09 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,6 +1,32 @@
-
-
 #ifndef VECTORLAB_VECTOR_H
 #define VECTORLAB_VECTOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+typedef struct vector {
+    int *data;
+    size_t size;
+    size_t capacity;
+} vector;
+
+//возвращает структуру-дескриптор вектор из n значений
+vector createVector(size_t n);
+
+//изменяет количество
+//памяти, выделенное под хранение элементов вектора
+void reserve(vector *v, size_t newCapacity);
+
+//удаляет элементы из контейнера, но не освобождает выделенную память
+void clear(vector *v);
+
+//освобождает память, выделенную под
+//неиспользуемые элементы
+void shrinkToFit(vector *v);
+
+//освобождает память, выделенную вектору
+void deleteVector(vector *v);
 
 #endif //VECTORLAB_VECTOR_H
